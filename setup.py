a5efb52d995cb2a0bc0c3aaa3904b8f0fab554c2
Dove <jinmoo21@naver.com>
2020-11-18 02:04:12 +0900

    dist

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='pwdriver',
-    version='0.1.1',
+    version='0.1.2',
     license='MIT',
     author='Jinmoo Han',
     author_email='jinmoo21@naver.com',
