f8544ab0264e68df669914ca97b3dae50814d211
Dove <jinmoo21@naver.com>
2021-10-28 21:09:00 +0900

    Update dependencies including selenium4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='pwdriver',
-    version='0.16.0',
+    version='0.17.0',
     license='MIT',
     author='Jinmoo Han',
     author_email='jinmoo21@naver.com',
